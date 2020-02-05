d1fd8833173d00968edea53e0b371c82577887ba
AgentPrus <nikitaprus98@gmail.com>
2020-02-05 15:57:14 +0200

    change markup by using bootstrap

--- index.php
+++ index.php
@@ -18,69 +18,69 @@ print_r($inputData);
 ?>
 
 <?php include('include/header.inc.php'); ?>
-    <form action="index.php" method="post">
-        <fieldset>
-            <div class="container">
-                <div class="form-group">
-                    <label class="col-form-label">First Name
-                        <input type="text" class="form-control" placeholder="First Name" name="firstName">
-                    </label>
+    <div class="container">
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label class="col-sm-2 col-form-label">First Name</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" placeholder="First Name" name="firstName">
                 </div>
             </div>
-            <div class="container">
-                <div class="form-group">
-                    <label class="col-form-label">Last Name
-                        <input type="text" class="form-control" placeholder="Last Name" name="lastName">
-                    </label>
+            <div class="form-group">
+                <label class="col-sm-5 col-form-label">Last Name</label>
+                <div class="col-sm-5 col-form-label">
+                    <input type="text" class="form-control" placeholder="Last Name" name="lastName">
                 </div>
             </div>
-            <div class="container">
-                <div class="form-group">
-                    <label class="col-form-label">Data of Birth
-                        <input type="date" class="form-control" placeholder="Data of Birth" name="dateOfBirth">
-                    </label>
+            <div class="form-group">
+                <label class="col-sm-5 col-form-label">Data of Birth</label>
+                <div class="col-sm-5 col-form-label">
+                    <input type="date" class="form-control" placeholder="Data of Birth" name="dateOfBirth">
                 </div>
             </div>
 
-            <div class="container">
-                <fieldset>
-                    <legend>Are you working now?</legend>
-                    <label class="form-check-label">
-                        <input type="radio" name="options" value="Yes" checked="checked"> Yes
-                    </label>
-                    <label class="orm-check-label">
-                        <input type="radio" name="options" value="No"> No
-                    </label>
-                </fieldset>
+            <fieldset class="form-group col">
+                <legend>Are you working now?</legend>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="options" value="No" checked>
+                    <label class="form-check-label">Yes</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="options" value="No">
+                    <label class="form-check-label">No</label>
+                </div>
+            </fieldset>
+            <fieldset class="form-group col">
+                <legend>Please Choose Technologies Which You Know</legend>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="check_boxes_list[]" value="JS">
+                    <label class="form-check-label">JS</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="check_boxes_list[]" value="CSS">
+                    <label class="form-check-label">CSS</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="check_boxes_list[]" value="HTML">
+                    <label class="form-check-label">HTML</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="check_boxes_list[]" value="PHP">
+                    <label class="form-check-label">PHP</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" name="check_boxes_list[]" value="NODEJS">
+                    <label class="form-check-label">NODE JS</label>
+                </div>
+            </fieldset>
+            <div class="form-group col">
+                <label>Personal info info</label>
+                <textarea class="form-control" name="personalInfo" rows="3"></textarea>
             </div>
-            <div class="container form-check">
-                <fieldset>
-                    <legend>Please Choose Technologies Which You Know</legend>
-                    <label class="form-check">
-                        <input type="checkbox" name="check_boxes_list[]" value="JS">JS<br>
-                    </label>
-                    <label class="form-check">
-                        <input type="checkbox" name="check_boxes_list[]" value="CSS">CSS<br>
-                    </label>
-                    <label class="form-check">
-                        <input type="checkbox" name="check_boxes_list[]" value="HTML">HTML<br>
-                    </label>
-                    <label class="form-check">
-                        <input type="checkbox" name="check_boxes_list[]" value="PHP">PHP<br>
-                    </label>
-                    <label class="form-check">
-                        <input type="checkbox" name="check_boxes_list[]" value="NODEJS">NODE JS<br>
-                    </label>
-                </fieldset>
+
+            <div class="form-group col">
+                <input type="submit" class="btn-dark">
             </div>
-            <div class="container form-group">
-                <label>Personal info info
-                    <textarea class="form-control" name="personalInfo" rows="3"></textarea>
-                </label>
-            </div>
-        </fieldset>
-        <div class="container">
-            <input type="submit" class="btn-dark">
-        </div>
-    </form>
+        </form>
+    </div>
 <?php include('include/footer.inc.php'); ?>
